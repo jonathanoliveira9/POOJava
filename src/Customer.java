24902e29fcd9b2b828db6ce0164cd52439c0dcdd
jonathan.silva.impulso <jonathan.silva.impulso@vindi.com.br>
2025-08-21 22:47:09 -0300

    add

---
+++ src/Customer.java
@@ -0,0 +1,13 @@
+public class Customer {
+    private String name;
+    private String document;
+    private String email;
+    private char sex;
+    private String address;
+    private String phone;
+    private String city;
+
+    boolean canBuyTickets() {
+        return true;
+    }
+}
