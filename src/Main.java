e1048aa7ffa0a7aacb795a992f9ee4606c0ee90d
jonathan.silva.impulso <jonathan.silva.impulso@vindi.com.br>
2025-08-23 16:10:09 -0300

    update.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Seller seller = new Seller();
         Customer customer = new Customer();
         System.out.println(customer.canBuyTickets());
         customer.setAddress("O'Connell Street");
@@ -11,6 +12,12 @@ public class Main {
         customer.setAddress("O'Connell ");
         System.out.println(customer.getAddress());
 
+        System.out.println("Giving money to seller 50.0 dollars");
+        seller.receiveMoney(50.0);
+        System.out.println("Seller has " + seller.showsSalary() + "dollars");
+        System.out.println("Giving money to seller 1456.1 dollars");
+        seller.receiveMoney(1456.1);
+        System.out.println("Seller has " + seller.showsSalary() + "dollars");
     }
 }
 
