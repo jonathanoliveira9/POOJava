055f3b9a64e5c8b8c334c96a8c10397e0c95e7ae
jonathan.silva.impulso <jonathan.silva.impulso@vindi.com.br>
2025-08-29 16:41:57 -0300

    Add functionality to calculate and compare the areas of two triangles based on user input with using POO

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Scanner sc = new Scanner(System.in);
+        double xA, xB, xC, yA, yB, yC;
+        System.out.println("Enter the measure of triangle X: ");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+
+        System.out.println("Enter the measure of triangle Y: ");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2.0;
+
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaY == areaX) {
+            System.out.println("Triangle X and Y have the same area");
+        }
+        if (areaX > areaY) {
+            System.out.println("Triangle X has the greater area");
+        } else {
+            System.out.println("Triangle Y has the greater area");
+        }
+
+        sc.close();
     }
 }
 
