fc0cc949c8290eb177ef856b07488e4bb94c9b41
jonathan.silva.impulso <jonathan.silva.impulso@vindi.com.br>
2025-08-31 13:43:58 -0300

    create function to calculate area

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Locale.setDefault(Locale.US);
+
         Scanner sc = new Scanner(System.in);
         Triangle x, y;
         x = new Triangle();
@@ -20,13 +23,8 @@ public class Main {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2.0;
-
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-
+        double areaX = x.area();
+        double areaY = y.area();
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
 
