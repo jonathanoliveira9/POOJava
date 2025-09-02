c3ec989535a5857fcf63126e8f552eaeec2b32d4
jonathan.silva.impulso <jonathan.silva.impulso@vindi.com.br>
2025-09-02 16:36:48 -0300

    Refactor Product class to encapsulate fields and add getter/setter methods; update Program to utilize new methods for product initialization

--- src/Program.java
+++ src/Program.java
@@ -14,6 +14,7 @@ public class Program {
         double price = sc.nextDouble();
 
         Product product = new Product(name, price);
+        product.setName(name);
 
         System.out.println("Product: data " + product);
 
